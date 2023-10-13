a22bb82d097d1fb05f3a3560a27e9728f4d22dc6
Elchin <elcinceferli@gmail.com>
2023-10-13 08:29:56 -0700

    change lu to zu

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,14 +8,10 @@
 
 int main(void)
 {
-	char a;
-	int b;
-	float c;
-
-	printf("Size of a char: %lu byte(s)\n", sizeof(a));
-	printf("Size of an int: %lu byte(s)\n", sizeof(b));
-	printf("Size of a long int: %lu byte(s)\n", (long int)sizeof(b));
-	printf("Size of a long long int: %lld byte(s)\n", (long long int)sizeof(b));
-	printf("Size of a float: %lu byte(s)\n", sizeof(c));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
