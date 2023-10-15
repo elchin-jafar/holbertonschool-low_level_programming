d9db92fe1b5982913d8b49c0896650cd55c25db8
Elchin <elcinceferli@gmail.com>
2023-10-15 16:56:57 -0700

    add is in messages

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -15,9 +15,9 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	result = n % 10;
 	if (result > 5)
-		printf("Last digit of %d is %d and greater than 5\n", n, result);
+		printf("Last digit of %d is %d and is greater than 5\n", n, result);
 	else if (result < 6 && result != 0)
-		printf("Last digit of %d is %d and less than 6 and not 0\n", n, result);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, result);
 	else
 		printf("Last digit of %d is %d and is 0\n", n, result);
 
