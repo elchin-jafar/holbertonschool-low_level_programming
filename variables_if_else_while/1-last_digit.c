ab57b692a57a74d8b2bced3740e386512b7a125e
Elchin <elcinceferli@gmail.com>
2023-10-15 16:55:38 -0700

    find if last digit of given number higher than 5 or lower than 6 or exaclty zero

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int result;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	result = n % 10;
+	if (result > 5)
+		printf("Last digit of %d is %d and greater than 5\n", n, result);
+	else if (result < 6 && result != 0)
+		printf("Last digit of %d is %d and less than 6 and not 0\n", n, result);
+	else
+		printf("Last digit of %d is %d and is 0\n", n, result);
+
+	return (0);
+}
--- variables_if_else_while/README.md
+++ variables_if_else_while/README.md
@@ -1,2 +1,3 @@
 C - VARIABLES, IF, ELSE, WHILE <br />
-0 - write if random number positve, negative or zero
+0 - write if random number positve, negative or zero <br />
+1 - write last digit of given number and say if its bigger than 5, or smaller than 6, or its exactly 0
