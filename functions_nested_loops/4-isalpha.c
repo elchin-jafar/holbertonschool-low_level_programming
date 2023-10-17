2f8c9d950171c71537a075218d97e5f770c5b756
Elchin <elcinceferli@gmail.com>
2023-10-17 06:48:02 -0700

    find alpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if given character is in alphabet
+ *
+ * Return: 1 if alphabet, 0 if not
+ */
+
+int _isalpha(int c)
+{
+	if ((c > 64 && c < 91) || (c > 96 && c < 123))
+		return (1);
+	return (0);
+}
