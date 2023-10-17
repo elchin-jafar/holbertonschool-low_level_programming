228bcc913fd890a53e6da8faedc3c80eb41c8b90
Elchin <elcinceferli@gmail.com>
2023-10-17 03:41:16 -0700

    print alphabet 10 times

---
+++ functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "1-alphabet.c"
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Return: Always void
+ */
+
+void print_alphabet_x10(void)
+{
+	int counter;
+	void print_alphabet(void);
+
+	for (counter = 0; counter < 10; counter++)
+	{
+		print_alphabet();
+	}
+}
