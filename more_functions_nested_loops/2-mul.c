5492690d9d12917797a7af3de8e412d6a68b3e70
Elchin <elcinceferli@gmail.com>
2023-10-19 08:49:37 -0700

    multiply 2 inputs

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - take 2 input and multiply them
+ * @a: first member
+ * @b: second member
+ * Return: muliply of members
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
