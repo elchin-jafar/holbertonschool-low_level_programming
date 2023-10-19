a384ff5cb53b2bd68933a50d49e8107312eee878
Elchin <elcinceferli@gmail.com>
2023-10-19 14:35:44 -0700

    add fizzbuzz for 15

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,10 +13,12 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
-			printf("Fizz ");
+		if (i % 15 == 0)
+			printf("FizzBuzz ");
 		else if (i % 5 == 0)
 			printf("Buzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
 		else
 			printf("%d ", i);
 	}
