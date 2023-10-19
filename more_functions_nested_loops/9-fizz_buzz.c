13f650aa3159fb000462a3ca1b42f0b6a439d2eb
Elchin <elcinceferli@gmail.com>
2023-10-19 14:40:22 -0700

    change whitespace logic

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,13 +14,16 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 15 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else
-			printf("%d ", i);
+			printf("%d", i);
+		if (i == 100)
+			continue;
+		printf(" ");
 	}
 	printf("\n");
 	return (0);
