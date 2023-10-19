1c32bbd04a4ef2fc5f74eb68755e99302c083c28
Elchin <elcinceferli@gmail.com>
2023-10-19 14:17:35 -0700

    fizz-buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints nums in 1-100 range, Fizz instead of multiples of 3,
+ * Buzz instead of multiples of 5
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
