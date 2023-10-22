9d04080f3c6b2ddd0ce2e7e34ea36ec538302113
Elchin <elcinceferli@gmail.com>
2023-10-22 12:21:32 -0700

    change limits

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 < 100; num1++)
+	for (num1 = 0; num1 < 99; num1++)
 	{
-		for (num2 = 1; num2 < 101; num2++)
+		for (num2 = num1 + 1; num2 < 100; num2++)
 		{
 			putchar(num1 / 10 + '0');
 			putchar(num1 % 10 + '0');
@@ -19,7 +19,7 @@ int main(void)
 			putchar(num2 / 10 + '0');
 			putchar(num2 % 10 + '0');
 
-			if (num1 == 99 && num2 == 100)
+			if (num1 == 98 && num2 == 99)
 				continue;
 			putchar(',');
 			putchar(' ');
