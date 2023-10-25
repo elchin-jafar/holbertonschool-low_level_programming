5cfc04d60f19e5528f75ac50b207ed9d2a76655d
Elchin <elcinceferli@gmail.com>
2023-10-25 06:37:55 -0700

    consider odd length

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -7,12 +7,19 @@
 
 void puts_half(char *str)
 {
-	int length = 0, i;
+	int length = 0, i, start;
 
 	while (*(str + length))
 		length++;
 
-	for (i = length / 2; i < length; i++)
+	if (length % 2 == 0)
+	{
+		start = length / 2;
+	}
+	else
+		start = (length + 1) / 2;
+
+	for (i = start; i < length; i++)
 		_putchar(*(str + i));
 
 	_putchar('\n');
