ee24f39f7c42701b623c29d78aae7a160e7edd24
Elchin <elcinceferli@gmail.com>
2023-10-25 07:07:32 -0700

    fix bug for whitespace

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -14,8 +14,9 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		if (i == n - 1)
-			printf("%d\n", *(a + i));
+			printf("%d", *(a + i));
 		else
 			printf("%d, ", *(a + i));
 	}
+	printf("\n");
 }
