9f7a73781b40d3583b592a2b5856de29c7cdf264
Elchin <elcinceferli@gmail.com>
2023-10-25 09:27:44 -0700

    add null terminator to dest

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -7,7 +7,7 @@
  * Return: pointer to dest
  */
 
-char *_strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
@@ -16,5 +16,7 @@ char *_strcpy(char *dest, const char *src)
 		*(dest + i) = *(src + i);
 		i++;
 	}
+	*(dest + i) = '\0';
+
 	return (dest);
 }
