cd9508c1ed17e602e9481a7f6bba35ca5ecf6476
Elchin <elcinceferli@gmail.com>
2023-10-26 15:57:42 -0700

    consider first char

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -28,7 +28,8 @@ char *cap_string(char *str)
 				str[i - 1] == '{' ||
 				str[i - 1] == '}' ||
 				str[i - 1] == '\n' ||
-				str[i - 1] == '\t'
+				str[i - 1] == '\t' ||
+				i == 0
 		   )
 			str[i] -= 32;
 		 i++;
