d5f9714e73d4fe41dca517ae13e830894b6383a7
Elchin <elcinceferli@gmail.com>
2023-10-27 17:46:48 -0700

    strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - locates char in given string
+ * @s: given string
+ * @c: given char
+ * Return: char or Null
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (&s[i]);
+		i++;
+	}
+	return ('\0');
+}
--- pointers_arrays_strings/README.md
+++ pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 POINTERS TO POINTERS, 2D ARRAYS
 0 - fill memory with constant byte <br />
 1 - copy from source memo to final dest memo <br />
+2 - locates character in a string <br />
