36c4f494696a95c319681c8bee7e23e142ddebd6
Elchin <elcinceferli@gmail.com>
2023-11-01 05:16:18 -0700

    correct typo on guards

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,5 +1,25 @@
-#IFNDEF MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
+int _isupper(int c);
+char *_memset(char *s, char b, unsigned int n);
+char *_strcat(char *dest, char *src);
+int _isdigit(int c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strncat(char *dest, char *src, int n);
+int _atoi(char *s);
+char *_strchr(char *s, char c);
+int _strlen(char *s);
+char *_strncpy(char *dest, char *src, int n);
+int _islower(int c);
+void _puts(char *s);
+int _strcmp(char *s1, char *s2);
+unsigned int _strspn(char *s, char *accept);
+int _isalpha(int c);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+int _abs(int n);
+char *_strcpy(char *dest, char *src);
 
-
-#ENDIF
+#endif
