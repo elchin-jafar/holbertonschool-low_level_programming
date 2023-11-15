7caa5e81c82fc6a91849874da8feb42139460292
Elchin <elcinceferli@gmail.com>
2023-11-15 05:55:43 -0800

    fix bugs

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * string_nconcat - concat 2 string
@@ -13,24 +12,29 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	unsigned int length1, index, index_str = 0;
+	unsigned int length = n, index;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	length1 = strlen(s1);
+	for (index = 0; s1[index]; index++)
+		length++;
 
-	ptr = malloc((length1 + n) * sizeof(char));
+	ptr = malloc((length + 1) * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
 
-	for (index = 0; index < length1; index++)
-		ptr[index_str++] = s1[index];
+	length = 0;
 
-	for (index = 0; index < n; index++)
-		ptr[index_str++] = s2[index];
+	for (index = 0; s1[index]; index++)
+		ptr[length++] = s1[index];
+
+	for (index = 0; s2[index] && index < n; index++)
+		ptr[length++] = s2[index];
+
+	ptr[length] = '\0';
 
 	return (ptr);
 }
