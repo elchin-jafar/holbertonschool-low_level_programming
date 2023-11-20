d377e8da455931f034c2c5ee76d8a9f07ccf713e
Elchin <elcinceferli@gmail.com>
2023-11-20 08:10:24 -0800

    fix age 0 condition

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,10 +13,9 @@ void print_dog(struct dog *d)
 		if (d->name == NULL)
 			printf("Name: (nil)\n");
 		else
+		{
 			printf("Name: %s\n", d->name);
-		if (d->age == 0)
-			printf("Age: (nil)\n");
-		else
+		}
 			printf("Age: %.6f\n", d->age);
 		if (d->owner == NULL)
 			printf("Owner: (nil)\n");
