77fb65ff8e42d47431114c4140ad52e9d16b0f18
Elchin <elcinceferli@gmail.com>
2023-11-20 07:32:33 -0800

    function that initialize a var of type struct dog

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - initializing struct dog d with given info
+ * @d: structure to initialize
+ * @name: given name to pass to d
+ * @age: given age to pass to d
+ * @owner: given owner to pass to d
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- structures_typedef/README.md
+++ structures_typedef/README.md
@@ -1,2 +1,3 @@
 STRUCTURES & TYPEDEF <br />
 0 - define simple struct named dawg which has name, age, owner fields <br />
+1 - function that initialize a var of type struct dog <br >
