aa339ae57b572a63ec2ab5aa51fcc4d2648cac97
Elchin <elcinceferli@gmail.com>
2023-11-20 07:15:04 -0800

    struct of dog

---
+++ structures_typedef/README.md
@@ -0,0 +1,2 @@
+STRUCTURES & TYPEDEF <br />
+0 - define simple struct named dawg which has name, age, owner fields <br />
---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - simple struct
+ * @name: name of dawg
+ * @age: age of dawg
+ * @owner: owner of dawg
+ *
+ * Description: structure of the best dawg in da world
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
