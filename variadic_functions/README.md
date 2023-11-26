f503058e4cf9dc9d15575fc7dc11944506eba8d4
Elchin <elcinceferli@gmail.com>
2023-11-26 11:52:28 -0800

    print nums

--- variadic_functions/README.md
+++ variadic_functions/README.md
@@ -1,2 +1,3 @@
 VARIADIC FUNCTIONs <br />
 0 - sum them all <br />
+1 - print numbs <br />
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,6 +4,13 @@
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+
+/**
+ * struct format_types - Struct format_types
+ * @identifier: The conversion specifier
+ * @f: The function pointer
+ */
 
 typedef struct format_types
 {
