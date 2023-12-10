9533b447a03749e5883c0dc0431003d0df69dba7
Elchin <elcinceferli@gmail.com>
2023-12-10 11:11:45 -0800

    flip bits

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -10,7 +10,7 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsidgned long int xor = n ^ m, bits = 0;
+	unsigned long int xor = n ^ m, bits = 0;
 
 	while (xor > 0)
 	{
