6e7c141111169374631e27a06771161645abd850
Elchin <elcinceferli@gmail.com>
2023-12-09 17:44:02 -0800

    add new node to the beginning

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node - Add new node to the beginning of given linked list
+ * @head: pointer to the head of given list
+ * @str: string have to add to the beginning
+ *
+ * Return: address of new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	char *dup;
+	int len;
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	dup = strdup(str);
+
+	if (dup == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = dup;
+	new->len = len;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}
